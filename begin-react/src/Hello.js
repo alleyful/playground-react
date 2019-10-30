e7dff181f67b2e93792b80adcbf61962e2260f78
alleyful <alleyful@naver.com>
2019-10-31 01:08:57 +0900

    [begin-react] props

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Hello() {
+function Hello({ name, color }) {
 
-	return <div>안녕하세요</div>
+	return <div style={{ color }}>안녕하세요 {name}</div>
+}
+
+Hello.defaultProps = {
+	name: '이름없음'
 }
 
 export default Hello;
