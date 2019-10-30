175105d79a16aa80a792a222bf0bb6a302f8b65b
alleyful <alleyful@naver.com>
2019-10-31 00:34:57 +0900

    [begin-react] 주석, 스타일, 변수

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 주석은 화면에 보이지 않습니다 */}
+      /* 중괄호로 감싸지 않으면 화면에 보입니다 */
+      <Hello
+        // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
