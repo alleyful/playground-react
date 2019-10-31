878e3fd163ad0f235c077810bfce26bc9185a6ee
alleyful <alleyful@naver.com>
2019-10-31 10:42:29 +0900

    [begin-react] useState

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React 스터디 및 정리를 위한 공간입니다.
 
+## 1장 리액트 입문
+[begin-react](./begin-react)
+
--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from "./Counter";
 
 function App() {
 
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial/>
-      <Hello color="pink"/>
-    </Wrapper>
+    <Counter />
   );
 }
 
