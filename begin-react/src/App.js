209abecd6392a689086dec03cfda1bf981edfe3f
alleyful <alleyful@naver.com>
2019-11-04 13:48:58 +0900

    [begin-react] useCallback

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
@@ -41,7 +41,7 @@ function App() {
 	]);
 
 	const nextId = useRef(4);
-	const onCreate = () => {
+	const onCreate = useCallback(() => {
 		const user = {
 			id: nextId.current,
 			username,
@@ -55,21 +55,21 @@ function App() {
 			email: ''
 		});
 		nextId.current += 1;
-	};
+	}, [ users, username, email ]);
 
-	const onRemove = id => {
+	const onRemove = useCallback(id => {
 		setUsers(users.filter(user => user.id !== id));
-	};
+	}, [ users ]);
 
-	const onToggle = id => {
+	const onToggle = useCallback(id => {
 		setUsers(
 			users.map(user =>
 				user.id === id ? { ...user, active: !user.active } : user
 			)
 		)
-	};
+	}, [ users ]);
 
-	const count = useMemo(() => countActiveUsers(users), [users]);
+	const count = useMemo(() => countActiveUsers(users), [ users ]);
 
 	return (
 		<>
