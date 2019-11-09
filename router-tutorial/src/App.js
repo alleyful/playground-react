2ef31775f87a3c4b62863c663d952e18eeb4c59a
alleyful <alleyful@naver.com>
2019-11-10 02:15:32 +0900

    [router-tutorial] 부가기능 history, WithRouter, Switch, NacLink

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
 	return (
@@ -17,10 +18,24 @@ const App = () => {
 				<li>
 					<Link to="/profiles">프로필 목록</Link>
 				</li>
+				<li>
+					<Link to="/history">예제</Link>
+				</li>
 			</ul>
-			<Route path="/" exact={true} component={Home}/>
-			<Route path="/about" component={About}/>
-			<Route path="/profiles" component={Profiles}/>
+			<Switch>
+				<Route path="/" exact={true} component={Home}/>
+				<Route path="/about" component={About}/>
+				<Route path="/profiles" component={Profiles}/>
+				<Route path="/history" component={HistorySample}/>
+				<Route
+					render={({ location }) => (
+						<div>
+							<h2>이 페이지는 존재하지 않습니다:</h2>
+							<p>{location.pathname}</p>
+						</div>
+					)}
+				/>
+			</Switch>
 		</div>
 	);
 };
--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
 
 const Profiles = () => {
 	return (
@@ -8,10 +9,20 @@ const Profiles = () => {
 			<h3>유저목록</h3>
 			<ul>
 				<li>
-					<Link to="/profiles/velopert">velopert</Link>
+					<NavLink
+						to="/profiles/velopert"
+						activeStyle={{ background: 'black', color: 'white' }}
+					>
+						velopert
+					</NavLink>
 				</li>
 				<li>
-					<Link to="/profiles/gildong">gildong</Link>
+					<NavLink
+						to="/profiles/gildong"
+						activeStyle={{ background: 'black', color: 'white' }}
+					>
+						gildong
+					</NavLink>
 				</li>
 			</ul>
 
@@ -21,6 +32,7 @@ const Profiles = () => {
 				render={() => <div>유저를 선택해주세요.</div>}
 			/>
 			<Route path="/profiles/:username" component={Profile}/>
+			<WithRouterSample/>
 		</div>
 	);
 };
