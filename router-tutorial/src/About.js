e317f7f204f83184ab4656703ec3e91ecb333e63
alleyful <alleyful@naver.com>
2019-11-10 01:07:34 +0900

    [router-tutorial] params, query

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+	const query = qs.parse(location.search, {
+		ignoreQueryPrefix: true
+	});
+	const detail = query.detail === 'true';
+
 	return (
 		<div>
 			<h1>소개</h1>
 			<p>리액트 라우터 기초를 실습해보는 예제 입니다.</p>
+			{detail && <p>추가적인 정보...</p>}
 		</div>
 	);
 };
--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const App = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-      </ul>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-    </div>
-  );
+	return (
+		<div>
+			<ul>
+				<li>
+					<Link to="/">홈</Link>
+				</li>
+				<li>
+					<Link to="/about">소개</Link>
+				</li>
+			</ul>
+			<Route path="/" exact={true} component={Home}/>
+			<Route path="/about" component={About}/>
+			<Route path="/profiles/:username" component={Profile}/>
+		</div>
+	);
 };
 
 export default App;
