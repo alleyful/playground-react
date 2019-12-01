8965eb055c59a0f3f0dbbc77523d8c86813df3bf
alleyful <alleyful@naver.com>
2019-12-02 00:29:29 +0900

    redux-thunk 사용하기

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -6,6 +6,14 @@ const DECREASE = "DECREASE";
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+// getState를 쓰지 않는다면 굳이 파라미터로 받아올 필요 없습니다.
+export const increaseAsync = () => dispatch => {
+  setTimeout(() => dispatch(increase()), 1000);
+};
+export const decreaseAsync = () => dispatch => {
+  setTimeout(() => dispatch(decrease()), 1000);
+};
+
 //초기값
 const initialState = 0;
 
