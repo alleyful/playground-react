2d088f6c747a83b036dc7d195710c5a8bc1d3c3f
alleyful <alleyful@naver.com>
2019-12-02 00:19:34 +0900

    logger 사용

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -6,9 +6,13 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
