630750069182eae9eb8d4b2b19c5b013ec0b696b
alleyful <alleyful@naver.com>
2019-12-04 00:02:30 +0900

    CORS

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+//axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/' : 'https://api.velog.io/';
+
 export const getPosts = async () => {
-  const response = await axios.get("http://localhost:4000/posts");
+  const response = await axios.get("/posts");
   return response.data;
 };
 
 export const getPostById = async id => {
-  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  const response = await axios.get(`/posts/${id}`);
   return response.data;
 };
