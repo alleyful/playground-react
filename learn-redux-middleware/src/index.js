5cb96b197315d71c52e79884d9b83928c15b15bd
alleyful <alleyful@naver.com>
2019-12-04 00:14:11 +0900

    redux-saga

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -5,14 +5,16 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
 
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
 
 const store = createStore(
   rootReducer,
@@ -20,10 +22,14 @@ const store = createStore(
   composeWithDevTools(
     applyMiddleware(
       ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware,
       logger
     )
   )
 ); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야합니다.
 
 ReactDOM.render(
   <Router history={customHistory}>
@@ -34,7 +40,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
