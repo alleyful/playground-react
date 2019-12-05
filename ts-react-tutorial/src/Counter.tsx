88a342ba839e0d4adbf0375e8dbfeb83dc401248
alleyful <alleyful@naver.com>
2019-12-06 00:35:38 +0900

    상태관리

--- ts-react-tutorial/src/Counter.tsx
+++ ts-react-tutorial/src/Counter.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+}
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
 
   return (
     <div>
