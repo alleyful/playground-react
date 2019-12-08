de5e89cdda6b9c5ef13f8f0de906b0d8fb1fc9d9
alleyful <alleyful@naver.com>
2019-12-08 19:36:10 +0900

    ReducerSample useHooks

--- ts-react-tutorial/src/ReducerSample.tsx
+++ ts-react-tutorial/src/ReducerSample.tsx
@@ -1,54 +1,9 @@
-import React, { useReducer } from "react";
-
-type Color = "red" | "orange" | "yellow";
-
-type State = {
-  count: number;
-  text: string;
-  color: Color;
-  isGood: boolean;
-};
-
-type Action =
-  | { type: "SET_COUNT"; count: number }
-  | { type: "SET_TEXT"; text: string }
-  | { type: "SET_COLOR"; color: Color }
-  | { type: "TOGGLE_GOOD" };
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case "SET_COUNT":
-      return {
-        ...state,
-        count: action.count // count가 자동완성되며, number 타입인걸 알 수 있습니다.
-      };
-    case "SET_TEXT":
-      return {
-        ...state,
-        text: action.text // text가 자동완성되며, string 타입인걸 알 수 있습니다.
-      };
-    case "SET_COLOR":
-      return {
-        ...state,
-        color: action.color // color 가 자동완성되며 color 가 Color 타입인걸 알 수 있습니다.
-      };
-    case "TOGGLE_GOOD":
-      return {
-        ...state,
-        isGood: !state.isGood
-      };
-    default:
-      throw new Error("Unhandled action");
-  }
-}
+import React from "react";
+import { useSampleState, useSampleDispatch } from "./SampleContext";
 
 function ReducerSample() {
-  const [state, dispatch] = useReducer(reducer, {
-    count: 0,
-    text: "hello",
-    color: "red",
-    isGood: true
-  });
+  const state = useSampleState();
+  const dispatch = useSampleDispatch();
 
   const setCount = () => dispatch({ type: "SET_COUNT", count: 5 }); // count 를 넣지 않으면 에러발생
   const setText = () => dispatch({ type: "SET_TEXT", text: "bye" }); // text 를 넣지 않으면 에러 발생
