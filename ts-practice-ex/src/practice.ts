605ec9631613c0854548b85f9df547320ebe2c3c
alleyful <alleyful@naver.com>
2020-01-06 23:51:03 +0900

    practice

--- ts-practice-ex/src/practice.ts
+++ ts-practice-ex/src/practice.ts
@@ -1,19 +1,15 @@
-let count = 0; // 숫자
-count += 1;
-count = "갑자기 분위기 문자열"; // 이러면 에러가 납니다!
+function sum(x: number, y: number): number {
+  return x + y;
+}
 
-const message: string = "hello world"; // 문자열
+sum(1, 2);
 
-const done: boolean = true; // 불리언 값
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
 
-const numbers: number[] = [1, 2, 3]; // 숫자 배열
-const messages: string[] = ["hello", "world"]; // 문자열 배열
+const total = sumArray([1, 2, 3, 4, 5]);
 
-messages.push(1); // 숫자 넣으려고 하면.. 안된다!
-
-let mightBeUndefined: string | undefined = undefined; // string 일수도 있고 undefined 일수도 있음
-let nullableNumber: number | null = null; // number 일수도 있고 null 일수도 있음
-
-let color: "red" | "orange" | "yellow" = "red"; // red, orange, yellow 중 하나임
-color = "yellow";
-color = "green"; // 에러 발생!
+function returnNothing(): void {
+  console.log("I am just saying hello world");
+}
