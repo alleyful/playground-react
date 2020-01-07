e559631157c4c5be062e408e691cf88778536f55
alleyful <alleyful@naver.com>
2020-01-08 01:43:34 +0900

    ex queue

--- ts-practice-ex/src/practice.ts
+++ ts-practice-ex/src/practice.ts
@@ -1,8 +1,24 @@
-function merge<A, B>(a: A, b: B): A & B {
-  return {
-    ...a,
-    ...b
-  };
+class Queue<T> {
+  list: T[] = [];
+  get length() {
+    return this.list.length;
+  }
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+  dequeue() {
+    return this.list.shift();
+  }
 }
 
-const merged = merge({ foo: 1 }, { bar: 1 });
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
