7faf54d0202bde8b5c35dd592de610738bafe605
alleyful <alleyful@naver.com>
2020-01-08 01:41:41 +0900

    generics

--- ts-practice-ex/src/practice.ts
+++ ts-practice-ex/src/practice.ts
@@ -1,25 +1,8 @@
-interface Person {
-  name: string;
-  age?: number;
+function merge<A, B>(a: A, b: B): A & B {
+  return {
+    ...a,
+    ...b
+  };
 }
 
-interface Developer extends Person {
-  skills: string[];
-}
-
-const person: Person = {
-  name: "김사람",
-  age: 20
-};
-
-const expert: Developer = {
-  name: "김개발",
-  skills: ["javascript", "react"]
-};
-
-type People = Person[];
-const people: People = [person, expert];
-
-type Color = "red" | "orange" | "yellow";
-const color: Color = "red";
-const colors: Color[] = ["orange", "yellow"];
+const merged = merge({ foo: 1 }, { bar: 1 });
