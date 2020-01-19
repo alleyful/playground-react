1326a4452ed2b017ffc9038a70375e3bdbb5c4ed
alleyful <alleyful@naver.com>
2020-01-20 01:11:47 +0900

    useAddtodo

--- ts-todos/src/components/TodoInsert.tsx
+++ ts-todos/src/components/TodoInsert.tsx
@@ -1,13 +1,16 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
+import useAddTodo from "../hooks/useAddTodo";
 
 function TodoInsert() {
   const [value, setValue] = useState("");
+  const addTodo = useAddTodo();
+
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
-    // TODO: 커스텀 Hook 사용해서 새 항목 등록
+    addTodo(value);
     setValue("");
   };
 
