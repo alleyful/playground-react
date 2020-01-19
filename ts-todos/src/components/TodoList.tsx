f7b7261924fbaa8370d1d7d5c5c25a15f015ae48
alleyful <alleyful@naver.com>
2020-01-20 01:04:47 +0900

    add hooks

--- ts-todos/src/components/TodoList.tsx
+++ ts-todos/src/components/TodoList.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Todo } from "../modules/todos";
 import TodoItem from "./TodoItem";
+import useTodos from "../hooks/useTodos";
 
 function TodoList() {
-  const todos: Todo[] = []; // TODO: 커스텀 Hook 사용하여 조회
+  const todos = useTodos(); // TODO: 커스텀 Hook 사용하여 조회
 
   if (todos.length === 0) return <p>등록된 항목이 없습니다.</p>;
 
