fd145a1a20d3b97e81075f6d9cc406f8ee64da74
alleyful <alleyful@naver.com>
2020-01-20 01:19:55 +0900

    useTodoActions

--- ts-todos/src/components/TodoItem.tsx
+++ ts-todos/src/components/TodoItem.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./TodoItem.css";
 import { Todo } from "../modules/todos";
+import useTodoActions from "../hooks/useTodoActions";
 
 type TodoItemProps = {
   todo: Todo;
 };
 
 function TodoItem({ todo }: TodoItemProps) {
-  // TODO: 커스텀 Hook 사용해서 onToggle / onRemove 구현
+  const { onToggle, onRemove } = useTodoActions(todo.id);
+
   return (
     <li className={`TodoItem ${todo.done ? "done" : ""}`}>
-      <span className="text">{todo.text}</span>
-      <span className="remove">(X)</span>
+      <span className="text" onClick={onToggle}>
+        {todo.text}
+      </span>
+      <span className="remove" onClick={onRemove}>
+        (X)
+      </span>
     </li>
   );
 }
